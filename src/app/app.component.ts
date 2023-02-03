a49ae7f777776b07bb08b1c2bc003e3964b1d03d
Manoj Gowda <manojgowdakr28@gmail.com>
2023-02-03 15:17:15 +0530

    new: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloNetFlix';
+
+  public CreateButton() {
+    console.log("create Button Netflix");
+  }
+  public deleteButton() {
+    console.log("delete Button NetFlix");
+  }
+  public DownloadButton() {
+    console.log("download Button NetFlix");
+  }
+  public UpdateButton() {
+    console.log("update Button NetFlix");
+  }
+  public TimepassButton() {
+    console.log("Timepass Button NetFlix");
+  }
+  public PassTimeButton() {
+    console.log("PassTime Button NetFix");
+  }
+  public SameButton1() {
+    console.log("Same Button1 NetFlix");
+  }
+  public SameButton2() {
+    console.log("Same Button2 NetFlix");
+  }
 }
